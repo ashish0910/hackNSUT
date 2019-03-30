ffc313735aa6c6b66c55b89b930da8aea267b9c9
Jatin Kumar <jatin15011999@gmail.com>
2019-03-31 00:28:10 +0530

    landing page added

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,13 +1,3 @@
-// Copyright (c) 2018 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-PoseNet example using p5.js
-=== */
-
 let video;
 let poseNet;
 let poses = [];
